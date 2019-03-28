d2f13930557d669be2ea534244e566f7d77a0522
unknown <will.ediger@gmail.com>
2019-03-27 21:58:59 -0500

    added routing for /, /about, /contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Route path="/" component={Navigation} />
+    <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
+    <Route path="/contact" exact component={Contact} />
+    {/* <Route path="/items-list/:id" component={Item} /> */}
+
   </div>
 );
 
